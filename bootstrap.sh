ba83816df7ab2aa4b840f7809131c7fadfb7a8d3
Atarii <atarii@users.noreply.github.com>
2025-12-30 23:29:04 +0000

    Install Python and responder in bootstrap.sh
    Added installation of Python and responder package.

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,11 @@ apk del -r shadow
 apk add dropbear mtd-utils-ubi btop unudhcpd --no-cache
 rc-update add dropbear default
 
+# Install Python and responder
+apk add python3 py3-pip python3-dev build-base --no-cache
+ln -sf /usr/bin/python3 /usr/bin/python
+pip3 install --no-cache-dir responder
+
 # Clear apk cache
 rm -rf /var/cache/apk/*
 
