78e4f42ab458d1e4a020b78db14759cd7b7c2a70
Atarii <atarii@users.noreply.github.com>
2026-01-04 21:09:18 +0000

    Removed udhcp

--- rootfs.sh
+++ rootfs.sh
@@ -59,9 +59,6 @@ overlay() {
   ln -s "/etc/init.d/usb_gadget" \
     "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/usb_gadget"
 
-  ln -s "/etc/init.d/udhcpd" \
-    "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/udhcpd"
-
   chmod +x "$ROOTFS_WORKSPACE_MNT/etc/local.d/00-responder-certs.start"
 
   # Create responder directories in the final rootfs
